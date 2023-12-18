915f5b2db201ca5e89b6751ffa529df9cb6c7fb9
blbarr03 <bbarr0101@gmail.com>
2023-12-18 12:02:42 -0800

    adds route to return all users

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const birds = require('./routes/birds.js');
 const tasks = require('./routes/tasks.js');
 const bodyParser = require('body-parser');
 const db = require('./db.js')
+const User = require('./models/User.js')
 
 const app = express()
 const PORT = 8000
@@ -19,9 +20,12 @@ app.get('/',(req, res)=>{
 
 app.post('/database', async (req,res) =>{
         try {
-          console.log(req.body);
           const dbcon = await db()
-          res.send({"connectionStatus": dbcon.connection.readyState })
+          const data = await User.find({})
+          res.status(200)
+          .send(data)
+        
+        
         }catch(error){
           console.error(error);
           res.status(501)

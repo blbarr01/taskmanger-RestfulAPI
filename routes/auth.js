e13c1bfb306ec0029ce9a66159a39dbf936decf4
blbarr03 <bbarr0101@gmail.com>
2023-12-19 12:07:40 -0800

    add more routes

---
+++ routes/auth.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const router = express.Router()
+
+router.get('/login', (req, res)=>{
+
+})
+.post('/login', (req, res)=>{
+
+})
+.post('/register', (req, res)=>{
+
+})
--- routes/user.js
+++ routes/user.js
@@ -1,20 +1,46 @@
 const express = require('express')
 const router = express.Router()
-
-// middleware that is specific to this router
-router.use((req, res, next) => {
-  console.log('Time: ', Date.now())
-  next()
-})
+const db = require('../db.js')
+const User = require('../models/User.js')
 
 // define the home page routes
-router.get('/', (req, res) => {
-  res.send('user home page')
+router.get('/', async (req, res) => {
+  try {
+    const data = await User.find({})
+    res.send(data)
+    
+  } catch (error) {
+    res.status(500).send({
+      errmsg: "resource not found"
+    })
+  }
 })
-.post('/', (req,res)=>{
-    console.log(req.body);
-    res.send("new bird added")
+.get('/:id', async (req, res) => {
+  console.log(req.params.id);
+  try {
+    const data = await User.findById(req.params.id)
+    res.send(data)
+    
+  } catch (error) {
+    res.status(401).send({
+      errmsg: "no resource found"
+    })
+  }
+
 })
+
+.post('/', async (req,res)=>{
+  let user = req.body
+  try {
+    let status = await User.create(user)
+    res.status(201)
+    .send(status.$set("msg","resource successfully created"))
+  } catch (error) {
+    console.error(error);
+  }
+    
+})
+
 
 
 module.exports = router

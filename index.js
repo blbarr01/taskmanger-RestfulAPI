f5664ca13041835c711d2711f4548b12a7e71082
blbarr03 <bbarr0101@gmail.com>
2024-01-09 11:58:19 -0800

    migrates to vue front end

--- index.js
+++ index.js
@@ -9,40 +9,23 @@ const cookieParser = require('cookie-parser')
 const app = express()
 const PORT = 8000
 
-app.use(bodyParser.json())
-  .use(cookieParser())
-  .use(bodyParser.urlencoded({ extended: false }))
-  .use(express.static('public'))
-
+app.
+// add universal middleware
+use(bodyParser.json()).
+use(cookieParser()).
+use(bodyParser.urlencoded({ extended: false })).
+// serve front end files
+use(express.static(path.join(__dirname+'/client/dist')))
 // way to use routers as middle wear 
   .use('/api/tasks',tasks)
   .use('/api/auth', auth)
+  .get('/api/test/',(req, res)=>{
+    console.log('got a req');
+    res.json({
+      msg: 'were recieving you'
+    })
+  })
   //
-  .get('/',(req, res)=>{
-    let file = path.join(__dirname, "views/index.html")
-    res.sendFile(file,(err)=>{
-    err ? console.error( ) : console.log('file sent')
-    })
-  })
-  .get('/login',(req, res)=>{
-    let file = path.join(__dirname, "views/login.html")
-    res.sendFile(file,(err)=>{
-    err ? console.error( ) : console.log('file sent')
-    })
-  })
-  .get('/register',(req, res)=>{
-    let file = path.join(__dirname, "views/register.html")
-    res.sendFile(file,(err)=>{
-    err ? console.error( ) : console.log('file sent')
-    })
-  })
-  
-  .get('/tasks',(req, res)=>{
-    let file = path.join(__dirname, "views/tasks.html")
-    res.sendFile(file,(err)=>{
-    err ? console.error( ) : console.log('file sent')
-    })
-  })
   
 
   .listen(8000,async ()=>{

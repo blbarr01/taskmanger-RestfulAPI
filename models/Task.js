01187b590516b8cd21789c04844ccd8e64dd1f64
blbarr03 <bbarr0101@gmail.com>
2024-01-31 15:23:30 -0800

    updates task schema

--- models/Task.js
+++ models/Task.js
@@ -4,13 +4,18 @@ const {Schema, model} = mongoose
 const taskSchema = new Schema({
     user_id:Schema.Types.ObjectId,
     title: String,
-    notes:String, 
+    notes:[String], 
     completed_status: {
         required: false,
         type: Boolean,
         default: false
     },
     created_at:{
+        required: false,
+        type: Date,
+        default: Date.now
+    },
+    last_updated:{
         required: false,
         type: Date,
         default: Date.now

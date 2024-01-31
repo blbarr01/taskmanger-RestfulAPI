37656cddd7a8237d648dd7892a17a948b5db041e
blbarr03 <bbarr0101@gmail.com>
2024-01-31 14:30:45 -0800

    checks for token before moving to next middleware refractors if check

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -3,22 +3,28 @@ const jwt = require('jsonwebtoken');
 
 const verify_token = (req, res, next) =>{
     const {access_token} = req.cookies
-    if (access_token){
+    console.log("access token" + access_token);
+    if (!access_token){
+        res.status(400)
+        res.send({msg: "no access token found, you are not allowed"})
+   
+    }
+    
+    try {
         let tk = access_token.split(" ")[1]
-        try {
-            let decoded = jwt.verify(tk, process.env.JWT_SK)
-            console.log(decoded);
-            let u_id = decoded._id; 
-            console.log("object id: " + u_id);
-            res.set('x-id', u_id)
-        } catch (error) {
-            console.error();
-            res.send(new Error('error while parsing token'))
-        }
+        let decoded = jwt.verify(tk, process.env.JWT_SK)
+        console.log(decoded);
+        let u_id = decoded._id; 
+        console.log("object id: " + u_id);
+        res.set('x-id', u_id)
+        res.append('x-cookie-status', 'cookies read')
+        next()
+
+    } catch (error) {
+        console.error();
+        res.send(new Error('error while parsing token'))
     }
 
-    res.append('x-cookie-status', 'cookies read')
-    next()
 }
 
 module.exports = verify_token

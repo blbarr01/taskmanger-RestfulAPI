58084a799f69545230c2c9dac7d1ccb6483b1fb0
blbarr03 <bbarr0101@gmail.com>
2024-02-08 09:26:27 -0800

    displays all notes related to task

--- client/src/components/task.vue
+++ client/src/components/task.vue
@@ -1,7 +1,9 @@
 <template>
     <li class="border-2 rounded border-gray-400 my-2 w-1/4 pl-3 ">
         <h4 class="text-lg text-blue-600" >{{ props.title }}</h4>
-        <p>{{ props.notes }}</p>
+        <ul v-if="notes" class="mr-8 list-disc">
+            <li v-for="note in notes">{{note}}</li>
+        </ul>
         <p>{{ props.completed_status }}</p>
         <label for="completed">completed:</label>
         <input type="checkbox" name="completed" id="">

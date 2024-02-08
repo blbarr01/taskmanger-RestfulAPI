2aeb084064edf38209d7217eac10b6f49c91f8e9
blbarr03 <bbarr0101@gmail.com>
2024-02-08 09:25:12 -0800

    adds form to create new task

--- client/src/pages/dashboard.vue
+++ client/src/pages/dashboard.vue
@@ -1,18 +1,24 @@
 <template>
     <h2> task page</h2>
-    <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure obcaecati 
-        adipisci optio sapiente officia? Molestiae libero 
-        vitae quasi sit voluptatibus dolor similique quo quisquam.
-    </p>
-        
-    <Suspense>
-        <tasklist/>
-    </Suspense>
+    <main>
+        <Suspense>
+            <tasklist/>
+        </Suspense>
+        <TaskForm v-if="showForm"/>
+        <button type="button" @click="()=> showForm = !showForm"
+        class="bg-slate-300 text-blue-800 hover:bg-slate-200 hover:text-blue-900 
+        sticky bottom-2 right-2 text-5xl rounded-lg p-6 ">
+            new task
+        </button>
+    </main>        
 </template>
 
 <script setup>
+import TaskForm from '@/components/TaskForm.vue';
 import tasklist from '@/components/tasklist.vue';
+import { ref } from 'vue';
+const showForm = ref(false)
+
 
 </script>
 

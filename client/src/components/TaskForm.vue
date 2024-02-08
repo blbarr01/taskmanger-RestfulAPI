be7f4bb207f4f619febb25bf1407eb8062f73f33
blbarr03 <bbarr0101@gmail.com>
2024-02-08 09:27:02 -0800

    adds form to submit new task

---
+++ client/src/components/TaskForm.vue
@@ -0,0 +1,66 @@
+<template>
+    <div class="grid grid-cols-2 gap-4">
+        <form @submit.prevent="handleSubmit"
+            class="container border-2 border-slate-400 rounded flex flex-col my-4 py-4 gap-2 pl-4">
+            <label for="title" class="hidden">task name</label>
+            <input v-model="title" type="text" name="title" id="title" placeholder="task name" class="w-1/4 text-black">
+
+            <!-- display notes to users  -->
+            <ul v-if="notes" id="note-holder" class="list-disc ml-4">
+                <li v-for="n,index in notes" :key="index">
+                    {{n}} 
+                    <span class="text-red-600 rounded-md bg-slate-300 hover:bg-slate-200  hover:text-red-700 px-2 ml-1 text-xl cursor-pointer"
+                    @click="removeNote(n)">-</span>
+                </li>
+            </ul>
+            <!--field to add new notes  -->
+            <div class="flex flex-row gap-4">
+                <label for="note" class="hidden">notes</label>
+                <input type="text" name="note" id="note" placeholder="additional notes" v-model="newNote" class="text-black">
+                <button class="bg-slate-300 text-blue-800  hover:bg-slate-200 hover:text-blue-900 rounded-lg p-2"
+                @click="addNote">
+                    add note
+                </button>
+            </div>
+            <button type="submit"
+            class="bg-slate-300 text-blue-800  hover:bg-slate-200 hover:text-blue-900 rounded-lg py-4  w-1/4"> 
+                create task
+            </button>
+        </form>
+        <section id="feedback">
+            <p v-if="error">
+                {{error}}
+            </p>
+        </section>
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+const title = ref('')
+const newNote = ref('')
+const notes = ref([])
+const error = ref("")
+
+function addNote(){
+    if(!newNote.value){
+        return
+    }
+    notes.value.push(newNote.value)
+    newNote.value = ""
+}
+
+function removeNote(n){
+    notes.value = notes.value.filter((note) => note !== n)
+}
+
+async function handleSubmit() {
+    if(!title.value){
+        error.value = "all tasks must have a title"
+        return 
+    }
+    console.log('code shouldnt run unless you have a title');
+  }
+</script>
+
+<style></style>

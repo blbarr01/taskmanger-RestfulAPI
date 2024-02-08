e2ea6fd46de68a40c7e7992ff84bdd0b0e28190e
blbarr03 <bbarr0101@gmail.com>
2024-02-08 12:18:33 -0800

    abstracts task route handlers into controller file

---
+++ controllers/tasks.controller.js
@@ -0,0 +1,41 @@
+const Task = require('../models/Task.js');
+
+// post
+async function createTask (req,res){
+    // get user id 
+    let user = res.get("x-id")
+    res.removeHeader('x-id')
+
+    const task = req.body;
+    task['user_id'] = user
+    try {
+    const status = await Task.create(task)
+    console.log(status);
+    
+    } catch (error) {
+        res.send('error').status(500)
+        console.error(error);    
+        return
+    }
+
+    res.status(201)
+    .json({
+        user,
+        msg:'we got your post'
+    })
+}
+
+//get
+async function getTasks (req, res){
+    let user = res.get("x-id")
+    res.removeHeader('x-id')
+    console.log(user);
+    let data  = await Task.find().where("user_id").equals(user)
+    res.json(data)
+}
+
+
+module.exports = {
+    createTask,
+    getTasks
+}

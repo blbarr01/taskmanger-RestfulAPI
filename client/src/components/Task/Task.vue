478d551a990036c2f7f90a4abb015623d98eb14e
blbarr03 <bbarr0101@gmail.com>
2024-03-04 11:48:22 -0800

    implements provide inject pattern

--- client/src/components/Task/Task.vue
+++ client/src/components/Task/Task.vue
@@ -1,16 +1,16 @@
 <template>
     <li class="border-2 rounded border-gray-400 my-2 p-4 gap-4 flex flex-row">
         <div  class="info container m-auto border-blue-200 border-2 p-2">
-            <TaskHeader :title="props.task.title" @update="updateTitle"/>
-            <TaskDetails v-if="props.task.notes" :notes ="props.task.notes"/>
+            <TaskHeader :title="task.title" @update="updateTitle"/>
+            <TaskDetails v-if="task.notes" :notes ="task.notes"/>
         </div>
         
         <div id="options" class="container mx-auto flex flex-col gap-4 place-items-start pl-4">
-            <button type="button" @click="handleUpdate(props.task._id)"
+            <button type="button" @click="handleUpdate(task._id)"
             class="bg-blue-700 text-white hover:bg-green-700 hover:text-black rounded-lg py-4 px-2 w-1/3">
                 update
             </button>
-            <button type="button" @click="handleDelete(props.task._id)"
+            <button type="button" @click="handleDelete(task._id)"
             class="bg-slate-300 text-blue-800 hover:bg-red-600 hover:text-white rounded-lg py-4 px-2 w-1/3">
                 delete
             </button>
@@ -19,13 +19,15 @@
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { ref, provide, isReactive, toRef, isRef, toValue } from 'vue';
 import TaskHeader from './TaskHeader.vue';
 import TaskDetails from './TaskDetails.vue';
 
 const props = defineProps({task:Object})
 
-
+let task = toRef(props.task)
+console.log(isReactive(task), isRef(task));
+provide('task', task)
 
 async function handleDelete(id){
     let endpoint = `http://localhost:8000/api/tasks/${id}`
@@ -45,7 +47,8 @@ async function handleUpdate(){
 }
 
 function updateTitle(title){
-    props.task.title = title
+    console.log('update initiated');
+    console.log(toValue(task));
 }
 
 
--- client/src/components/Task/TaskHeader.vue
+++ client/src/components/Task/TaskHeader.vue
@@ -2,31 +2,33 @@
 <!-- title and respective input -->
     <h4 v-if="!showEdit" :class="[ 'text-blue-600', 'cursor-pointer']"
     @click="toggleEdit">
-        {{displayTitle }}
+        {{task.title}}
     </h4>
-    <input v-else type="text" ref="input" name="task-title" :placeholder="displayTitle" 
+    <input v-else type="text" ref="input" name="task-title" :placeholder="task.title" 
     :class="['text-black']" @blur="[updateTitle($event.target.value)]"/>
 </template>
 
 <script setup>
-    import {ref} from "vue";
-    const props = defineProps(['title'])
+    import {inject, ref} from "vue";
+    const task = inject('task')
     const emit = defineEmits(['update'])
-
-    const displayTitle = ref(props.title)
+    console.log('printing from header - ',task.value.title );
+   
     const input = ref(null)
     const showEdit = ref(false)
-
+    
+    const toggleEdit = () => showEdit.value = !showEdit.value 
+    
     function updateTitle(title){
         toggleEdit()
         if(!title){
             return
         }
-        displayTitle.value = title
+        task.value.title = title
+        console.log('update emits');
         emit('update', title)
     }
 
-    const toggleEdit = () => showEdit.value = !showEdit.value 
 </script>
 
 <style>
